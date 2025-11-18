05ba0e96a8f1e68051322b32507b8781abbaa453
pereiradevel <you@example.com>
2025-11-18 14:06:52 -0300

    18/11

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-// Pega o elemento da nav bar
-const navBar = document.getElementById('navbar'); // Substitua pelo ID real da sua nav bar
-const SCROLL_AMOUNT = 100; // Define quantos pixels o usuário precisa rolar para a mudança ocorrer
-const NORMAL_COLOR = 'rgb(141, 140, 206)'; // Cor normal da navbar
-const SCROLLED_COLOR = 'transparent'; // Exemplo de cor para quando estiver rolando (você pode ajustar)
-
-window.addEventListener('scroll', function() {
-    if (window.scrollY >= SCROLL_AMOUNT) {
-        navBar.style.backgroundColor = SCROLLED_COLOR;
-        navBar.style.transition = 'background-color 0.5s ease';
-    } else {
-        navBar.style.backgroundColor = NORMAL_COLOR;
-        navBar.style.transition = 'background-color 0.5s ease';
-    }
-});
+const botoes = document.querySelector('.link')
+botoes.addEventListener('click' , function(){
+   let nome = prompt("Digite seu nome: ")
+   alert(`"Seja bem vindo(a), ${nome}!"`)
+})
